40741e0d68b5ad535baca3e216e605abb5d70ae7
satzurajkumar <satzurajkumar@gmail.com>
2025-07-22 05:27:29 +0530

    minor ui update

---
+++ src/pages/styles/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-bg": "linear-gradient(to right, #183c4c, #5d697b)",
+			},
+		},
+	},
+	plugins: [],
+};
--- src/pages/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// tailwind.config.js
-module.exports = {
-	content: ["./src/**/*.{html,js}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
